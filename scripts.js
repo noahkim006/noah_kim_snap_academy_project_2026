c4b1fc8972ae37f1165151f1fab8c4c27bac329a
noahkim006 <noahkim0981@gmail.com>
2026-04-17 08:45:42 -0700

    added images to dataset, created inital loading function of cards holding data

--- scripts.js
+++ scripts.js
@@ -1,3 +1,28 @@
-import { ITEMS } from './data.js'
+import { ITEMS } from './data.js';
 
-console.log(ITEMS)
+document.addEventListener("DOMContentLoaded", initializeCards);
+
+function initializeCards() {
+    console.log("hi");
+
+    const cardContainer = document.getElementById("card-container");
+    const cardTemplate = document.querySelector(".card");
+
+    cardContainer.innerHTML = "";
+
+    for (let i = 0; i < ITEMS.length; i++) {
+        let currentVenue = ITEMS[i];
+
+        const card = cardTemplate.cloneNode(true);              //create clone of empty card div in html
+        card.style.display = "block";                           //set style of card from none to block (make it visible)
+        
+        const venueName = card.querySelector(".venue-name");    //fill in data from ITEMS list
+        venueName.textContent = currentVenue.name;
+        const image = card.querySelector(".image-container");
+        image.src = currentVenue.images[0];                     //use first image as main
+        
+        card.dataset.id = currentVenue.id;                      //set ID of div id need later for functions
+
+        cardContainer.appendChild(card);                        //add to parent div
+    }
+}
